393b06c0671bc37b4eb8ca3ef124759006746fb1
dlarrington <35306140+dlarrington@users.noreply.github.com>
2018-01-14 14:46:58 -0600

    website update

---
+++ FileReadMain.java
@@ -0,0 +1,24 @@
+/*
+ * Programmer: Dalton Larrington
+ * Date: 1-14-18
+ * Description: To read from a text file and print the line number of each line
+ */
+
+/*
+ * We did not go over file reading and writing for more then a day in Into to Java
+ */
+import java.io.*;
+
+public class FileReadMain {
+
+	public static void main(String[] args) {
+		
+		String file = "PrintHelloWorld.java";
+		
+		String line = null;
+		
+		
+
+	}
+
+}

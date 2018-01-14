1f05c077b2918712890e7a2674ea478577703ab6
dlarrington <35306140+dlarrington@users.noreply.github.com>
2018-01-14 14:20:31 -0600

    website update

---
+++ FindPrimeNumbers.java
@@ -0,0 +1,36 @@
+/*
+ * Programmer: Dalton Larrington
+ * Date: 1-14-18
+ * Description: To find all prime numbers below 50
+ */
+
+/* 
+ * I originally had a hard time finding the correct format to find the prime numbers. I had originally thought I could use array lists, which is true.
+ * But my knowledge of array lists is less then par, partly due to Kevin spending minimal time on this subject at the end of fall semester. 
+ */
+public class FindPrimeNumbers {
+
+	public static void main(String[] args) {
+		
+		int number;
+		
+		for (int i = 2; i <= 50; i++)
+			
+		{
+			
+			if(i % 2 != 0)
+				
+				return true;
+			
+			else {
+				
+				return false;
+				
+			}
+				
+				
+		}
+		
+	}
+
+}

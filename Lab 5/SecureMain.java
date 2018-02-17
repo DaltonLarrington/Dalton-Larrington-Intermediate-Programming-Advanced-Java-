0acb77c219cd7bc155982ee12afc3b2a7001c0e2
dlarrington <35306140+dlarrington@users.noreply.github.com>
2018-02-17 15:54:55 -0600

    website update

---
+++ Lab 5/SecureMain.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SecureMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("This program checks to see input is safe. Please enter some input: ");
+		
+		
+
+	}
+
+}
